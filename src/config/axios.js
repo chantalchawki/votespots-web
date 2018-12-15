87f0a7617e966b602ad79c14678f1c7102cda8d2
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-12-15 22:02:41 +0200

    Fix axios config

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.SERVER_URL || 'http://localhost:3000';
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3000';
 
